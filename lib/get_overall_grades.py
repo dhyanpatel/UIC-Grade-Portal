8394675f5ea90329135b90c17a729e5f9a465c0b
Deep <deeprp2@uic.edu>
2018-10-23 18:15:49 -0500

    Testing - added function file for getting grades

--- lib/get_overall_grades.py
+++ lib/get_overall_grades.py
@@ -1,6 +1,6 @@
 import xlrd
-from pprint import pprint
 import json
+
 
 def dictionary():
     file_location = "C:\\Users\\Deepp\\PycharmProjects\\CS141_GradeLookup\\lib\\final template.xlsx"
@@ -76,6 +76,12 @@ def dictionary():
 
     overallPER_in_Class = []
     final_grade_inClass = []
+
+    class_Avg_LabQuizes = 0
+    class_Avg_labInClass = 0
+    overall_ClassPER = 0
+    zyante_ClassAvg = 0
+    iClicker_AVG = 0
 
     for a in range(8, worksheet.nrows):
         code_NUM.append(worksheet.cell_value(a, 0))
@@ -176,73 +182,129 @@ def dictionary():
                     overallPER_in_Class.append(worksheet.cell_value(u, r))
                     final_grade_inClass.append(worksheet.cell_value(u, r + 1))
 
+    # for loop to round the decimals
+    for t in range(len(lab_Quizes_Avg)):
+        if lab_Quizes_Avg[t] == "":
+            continue
+        lab_Quizes_Avg[t] = round((lab_Quizes_Avg[t]), 2)
+        lab_Quizes_PER_Score[t] = round((lab_Quizes_PER_Score[t]), 1)
+        lab_InClass_5PER_Score[t] = round((lab_InClass_5PER_Score[t]), 2)
+        lab_inClass_Avg[t] = round((lab_inClass_Avg[t]), 2)
+        program1[t] = round((program1[t]), 2)
+        avg_4_All_Programs[t] = round((avg_4_All_Programs[t]), 2)
+        programs_30PER_score[t] = round((programs_30PER_score[t]), 2)
+        zyante_PER_Done[t] = round((zyante_PER_Done[t]), 2)
+        zyante_10PER_score[t] = round((zyante_10PER_score[t]), 2)
+        iClickers[t] = round((iClickers[t]), 2)
+        iClickers_5PER_Score[t] = round((iClickers_5PER_Score[t]), 2)
+        zyante_10PER_score[t] = round((zyante_10PER_score[t]), 2)
+        midterm1_Total[t] = round((midterm1_Total[t]), 2)
+        midterm1_10PER_Score[t] = round((midterm1_10PER_Score[t]), 2)
+        midterm2_Total[t] = round((midterm2_Total[t]), 2)
+        midterm2_15PER_Score[t] = round((midterm2_15PER_Score[t]), 2)
+        final_Total[t] = round((final_Total[t]), 2)
+        final_20PER_Score[t] = round((final_20PER_Score[t]), 2)
+        overallPER_in_Class[t] = round((overallPER_in_Class[t]), 2)
+
+        class_Avg_LabQuizes += lab_Quizes_Avg[t]
+        class_Avg_labInClass += lab_inClass_Avg[t]
+        overall_ClassPER += overallPER_in_Class[t]
+        zyante_ClassAvg += zyante_PER_Done[t]
+        iClicker_AVG += iClickers[t]
+
+    class_Avg_LabQuizes = round((class_Avg_LabQuizes / nrows), 2)
+    class_Avg_labInClass = round((class_Avg_labInClass / nrows), 2)
+    overall_ClassPER = round((overall_ClassPER / nrows), 2)
+    zyante_ClassAvg = round((zyante_ClassAvg / nrows), 2)
+    iClicker_AVG = round((iClicker_AVG / nrows), 2)
+
+    # define the dictionary
     students = {}
 
+    # add vectors to the dictionary
     for student in range(len(code_NUM)):
         if code_NUM[student] == "":
             continue
-
-        students[code_NUM[student]] = {'Lab1_Quiz': lab1_Quiz[student],
-                                       'Lab2_Quiz': lab2_Quiz[student],
-                                       'Lab3_Quiz': lab3_Quiz[student],
-                                       'Lab4_Quiz': lab4_Quiz[student],
-                                       'Lab6_Quiz': lab6_Quiz[student],
-                                       'Lab7_Quiz': lab7_Quiz[student],
-                                       'Lab8_Quiz': lab8_Quiz[student],
-                                       'lab9_Quiz': lab9_Quiz[student],
-                                       'lab11_Quiz': lab11_Quiz[student],
-                                       'lab12_Quiz': lab12_Quiz[student],
-                                       'lab13_Quiz': lab13_Quiz[student],
-                                       'lab14_Quiz': lab14_Quiz[student],
-                                       'lab_Quizes_5%_Score': lab_Quizes_PER_Score[student],
-                                       'lab_Quizes_Avg': lab_Quizes_Avg[student],
-                                       'lab1_inClass': lab1_inClass[student],
-                                       'lab2_inClass': lab2_inClass[student],
-                                       'lab3_inClass': lab3_inClass[student],
-                                       'lab4_inClass': lab4_inClass[student],
-                                       'lab6_inClass': lab6_inClass[student],
-                                       'lab7_inClass': lab7_inClass[student],
-                                       'lab8_inClass': lab8_inClass[student],
-                                       'lab9_inClass': lab8_inClass[student],
-                                       'lab11_inClass': lab8_inClass[student],
-                                       'lab12_inClass': lab8_inClass[student],
-                                       'lab13_inClass': lab8_inClass[student],
-                                       'lab14_inClass': lab8_inClass[student],
-                                       'lab_InClass_5%_Score': lab_InClass_5PER_Score[student],
-                                       'lab_inClass_Avg': lab_inClass_Avg[student],
-                                       'program1': program1[student],
-                                       'program2': program2[student],
-                                       'program3': program3[student],
-                                       'program4': program4[student],
-                                       'program5': program5[student],
-                                       'program6': program6[student],
-                                       'avg_4_All_Programs': avg_4_All_Programs[student],
-                                       'programs_30%_score': programs_30PER_score[student],
-                                       'zyante_%_Done': zyante_PER_Done[student],
-                                       'zyante_10%_score': zyante_10PER_score[student],
-                                       'iClickers': iClickers[student],
-                                       'iClickers_5%_Score': iClickers_5PER_Score[student],
-                                       'midterm1_inClass': midterm1_inClass[student],
-                                       'midterm1_Lab': midterm1_Lab[student],
-                                       'midterm1_Total': midterm1_Total[student],
-                                       'midterm1_10%_Score': midterm1_10PER_Score[student],
-                                       'midterm2_inClass': midterm2_inClass[student],
-                                       'midterm2_Lab': midterm2_Lab[student],
-                                       'midterm2_Total': midterm2_Total[student],
-                                       'midterm2_15%_Score': midterm2_15PER_Score[student],
-                                       'final_inClass': final_inClass[student],
-                                       'final_Lab': final_Lab[student],
-                                       'final_Total': final_Total[student],
-                                       'final_20%_Score': final_20PER_Score[student],
-                                       'overall%_in_Class': overallPER_in_Class[student],
-                                       'final_grade_inClass': final_grade_inClass[student],
+        students[code_NUM[student]] = {
+                                       'lab Quizes': {
+                                           'lab1 Quiz': lab1_Quiz[student],
+                                           'lab2 Quiz': lab2_Quiz[student],
+                                           'lab3 Quiz': lab3_Quiz[student],
+                                           'lab4 Quiz': lab4_Quiz[student],
+                                           'lab6 Quiz': lab6_Quiz[student],
+                                           'lab7 Quiz': lab7_Quiz[student],
+                                           'lab8 Quiz': lab8_Quiz[student],
+                                           'lab9 Quiz': lab9_Quiz[student],
+                                           'lab11 Quiz': lab11_Quiz[student],
+                                           'lab12 Quiz': lab12_Quiz[student],
+                                           'lab13 Quiz': lab13_Quiz[student],
+                                           'lab14 Quiz': lab14_Quiz[student],
+                                           'lab Quizzes Avg': lab_Quizes_Avg[student],
+                                           'lab Quizzes 5% Score': lab_Quizes_PER_Score[student],
+                                           'class Avg -> LabQuizzes': class_Avg_LabQuizes,
+                                       },
+                                       'lab grades': {
+                                           'lab1 inClass': lab1_inClass[student],
+                                           'lab2 inClass': lab2_inClass[student],
+                                           'lab3 inClass': lab3_inClass[student],
+                                           'lab4 inClass': lab4_inClass[student],
+                                           'lab6 inClass': lab6_inClass[student],
+                                           'lab7 inClass': lab7_inClass[student],
+                                           'lab8 inClass': lab8_inClass[student],
+                                           'lab9 inClass': lab9_inClass[student],
+                                           'lab11 inClass': lab11_inClass[student],
+                                           'lab12 inClass': lab12_inClass[student],
+                                           'lab13 inClass': lab13_inClass[student],
+                                           'lab14 inClass': lab14_inClass[student],
+                                           'labs InClass 5% Score': lab_InClass_5PER_Score[student],
+                                           'labs inClass Avg': lab_inClass_Avg[student],
+                                           'class Avg -> inClass Labs': class_Avg_labInClass,
+                                       },
+                                       'Programs': {
+                                           'Program 1': program1[student],
+                                           'Program 2': program2[student],
+                                           'Program 3': program3[student],
+                                           'Program 4': program4[student],
+                                           'Program 5': program5[student],
+                                           'Program 6': program6[student],
+                                           'All programs Avg': avg_4_All_Programs[student],
+                                           '30% of all Programs': programs_30PER_score[student],
+                                       },
+                                       'Zyante': {
+                                           'Zyante % Done': zyante_PER_Done[student],
+                                           '10% of Zyante % Done': zyante_10PER_score[student],
+                                           'Class Avg -> Zyante completion': zyante_ClassAvg,
+                                       },
+                                       'iClickers': {
+                                           'iClickers %': iClickers[student],
+                                           '5% of iClickers': iClickers_5PER_Score[student],
+                                           'Class Avg -> iClickers %': iClicker_AVG,
+                                       },
+                                       'Midterm 1': {
+                                           'Midterm1 inClass': midterm1_inClass[student],
+                                           'Midterm1 Lab': midterm1_Lab[student],
+                                           'Midterm1 Total %': midterm1_Total[student],
+                                           '10% of Midterm1': midterm1_10PER_Score[student],
+                                       },
+                                       'Midterm 2': {
+                                           'Midterm2 inClass': midterm2_inClass[student],
+                                           'Midterm2 Lab': midterm2_Lab[student],
+                                           'Midterm2 Total %': midterm2_Total[student],
+                                           '15% of Midterm2': midterm2_15PER_Score[student],
+                                       },
+                                       'Final': {
+                                           'Final inClass': final_inClass[student],
+                                           'Final Lab': final_Lab[student],
+                                           'Final Total': final_Total[student],
+                                           '20% of Final': final_20PER_Score[student],
+                                       },
+                                       'Overall Grade': {
+                                           'Overall % in class': overallPER_in_Class[student],
+                                           'Final grade in class': final_grade_inClass[student],
+                                           'Class Avg -> Overall %': overall_ClassPER,
                                        }
+        }
 
-    pprint(students)
     cs141grade = json.dumps(students)
 
     return cs141grade
-
-
-
-
